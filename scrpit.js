578892c0c14e8b5700ad8b638de8b4100b66890d
Sairacat <201769jzy@gmail.com>
2026-03-16 14:58:02 +0800

    Finished a basic user interface for my calculator.

---
+++ scrpit.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.input')
+buttons.forEach(button => button.classList.add('changeColor'))
+
+
+
+
+
+
+
+function sum(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}

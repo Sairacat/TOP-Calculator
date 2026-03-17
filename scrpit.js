ca018cb80a4f126624078e7383225be19fcf127a
Sairacat <201769jzy@gmail.com>
2026-03-17 16:22:14 +0800

    Finished the basic logic of sum, substract, multip ly and divide. At this point, my calculator works for the calculation between two numbers. Still struggling about the calculation between three or even more numbers.

--- scrpit.js
+++ scrpit.js
@@ -1,35 +1,70 @@
 const buttons = document.querySelectorAll('.input')
-const display = document.querySelector('.display')
+const display = document.querySelector('#display')
 const numberButtons = document.querySelectorAll('.number')
 const operatorButtons = document.querySelectorAll('.operator')
 const clearButton = document.querySelector('.clearButton')
+const equalButton = document.querySelector('.equal')
+
+let isCalculated = false
+let operator
+let num1
+let num2
 
 numberButtons.forEach(button => button.addEventListener('click', function(e) {
-    if(display.textContent === '0') {
-        display.textContent = '';
-        display.textContent = e.target.textContent;
+    if(display.value === '0') {
+        display.value = e.target.textContent;
     }else {
-        display.textContent += e.target.textContent
+        if(isCalculated) {
+            display.value = e.target.textContent
+            isCalculated = false
+        }else {
+            display.value += e.target.textContent
+        }
     }
 }))
 
 operatorButtons.forEach(button => button.addEventListener('click', function(e) {
-     display.textContent += e.target.textContent
+    let currentValue = display.value
+    let lastChar = currentValue.slice(-1)
+    const operator = ['+', '-', '*', '/']
+    if(operator.includes(lastChar)) {
+        return
+    }
+     display.value += e.target.textContent
+     isCalculated = false
 }))
 
 clearButton.addEventListener('click', function(e) {
-    display.textContent = '0'
+    display.value = '0'
 })
-display.textContent = '0'
-
-
 
 buttons.forEach(button => button.classList.add('changeColor'))
 
+equalButton.addEventListener('click', () => {
+    let currentValue = display.value
+    const currentArray = currentValue.split(/([\*\/\+-])/)
+    operator = currentArray[1]
+    num1 = Number(currentArray[0])
+    num2 = Number(currentArray[2])
+    let result = operate(operator,num1,num2)
+    display.value = result.toString()
+    isCalculated = true
+})
 
 
 
 
+function operate(operator, num1, num2) {
+    if(operator === '+') {
+        return sum(num1, num2)
+    }else if(operator === '-') {
+        return subtract(num1, num2)
+    }else if(operator === '*') {
+        return multiply(num1, num2)
+    }else if(operator === '/') {
+        return divide(num1, num2)
+    }
+}
 
 function sum(a, b) {
     return a + b
